be90b73f5e411e2e476fb603b07947b3056e7618
VUNDELA DINESH REDDY <dineshreddy496@gmail.com>
2021-07-05 23:47:21 +0530

    Update index.js

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -6,14 +6,16 @@ class Speedometer extends Component {
   state = {speed: 0}
 
   onAccelerate = () => {
+    const {speed} = this.state
     if (speed < 200) {
-      this.setState(prevState => ({speed: prevState.count + 10}))
+      this.setState(prevState => ({speed: prevState.speed + 10}))
     }
   }
 
   onApplyBreak = () => {
+    const {speed} = this.state
     if (speed > 0) {
-      this.setState(prevState => ({speed: prevState.count - 10}))
+      this.setState(prevState => ({speed: prevState.speed - 10}))
     }
   }
 
@@ -24,19 +26,27 @@ class Speedometer extends Component {
       <div className="container">
         <h1 className="title">Speedometer</h1>
         <div className="img-container">
-          <img src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
-            alt="speedometer" className="image"
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+            alt="speedometer"
+            className="image"
           />
         </div>
-        <h1 className="heading">
-          Speed is {speed}mph
-        </h1>
+        <h1 className="heading">Speed is {speed}mph</h1>
         <p className="description">Min Limit is 0mph, Max Limit is 200mph</p>
         <div>
-          <button type="button" className="accelerate-button" onClick={this.onAccelerate}>
+          <button
+            type="button"
+            className="accelerate-button"
+            onClick={this.onAccelerate}
+          >
             Accelerate
           </button>
-          <button type="button" className="apply-break-button" onClick={this.onApplyBreak}>
+          <button
+            type="button"
+            className="apply-break-button"
+            onClick={this.onApplyBreak}
+          >
             Apply Break
           </button>
         </div>
@@ -46,4 +56,3 @@ class Speedometer extends Component {
 }
 
 export default Speedometer
-
